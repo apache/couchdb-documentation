b4badd27a3c6afd7fa75f691cd0311019f7dcb34
Joan Touzet <wohali@apache.org>
2018-11-28 23:13:59 -0500

    Python formatting fixes from black

--- src/conf.py
+++ src/conf.py
@@ -16,12 +16,17 @@ import sys
 
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath('../ext'))
+sys.path.insert(0, os.path.abspath("../ext"))
 
 needs_sphinx = "1.2"
 
-extensions = ["sphinx.ext.todo", "sphinx.ext.extlinks", 'github',
-              'httpdomain', 'configdomain']
+extensions = [
+    "sphinx.ext.todo",
+    "sphinx.ext.extlinks",
+    "github",
+    "httpdomain",
+    "configdomain",
+]
 
 source_suffix = ".rst"
 
@@ -31,12 +36,9 @@ nitpicky = True
 version = '2.3'
 release = '2.3.0'
 
-project = 'Apache CouchDB'
+project = "Apache CouchDB"
 
-copyright = '%d, %s' % (
-    datetime.datetime.now().year,
-    'Apache Software Foundation'
-)
+copyright = "%d, %s" % (datetime.datetime.now().year, "Apache Software Foundation")
 
 primary_domain = "http"
 
@@ -45,21 +47,15 @@ pygments_style = "sphinx"
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-html_theme_options = {
-    'canonical_url': "http://docs.couchdb.org/en/stable/"
-}
+html_theme_options = {"canonical_url": "http://docs.couchdb.org/en/stable/"}
 
 templates_path = ["../templates"]
 
 html_static_path = ["../static"]
 
-html_title = ' '.join([
-    project,
-    version,
-    'Documentation'
-])
+html_title = " ".join([project, version, "Documentation"])
 
-#html_style = "css/rtd_theme.css"
+# html_style = "css/rtd_theme.css"
 
 html_logo = "../images/logo.png"
 
@@ -67,61 +63,55 @@ html_favicon = "../images/favicon.ico"
 
 html_use_index = False
 
-html_additional_pages = {
-    'download': 'pages/download.html',
-    'index': 'pages/index.html'
-}
+html_additional_pages = {"download": "pages/download.html", "index": "pages/index.html"}
 
 html_context = {
     "ga_code": "UA-658988-6",
     # Enable the "Edit in GitHub link within the header of each page.
-    'display_github': True,
-    # Set the following variables to generate the resulting github URL for each page. 
+    "display_github": True,
+    # Set the following variables to generate the resulting github URL for each page.
     # Format Template: https://{{ github_host|default("github.com") }}/{{ github_user }}/{{ github_repo }}/blob/{{ github_version }}{{ conf_py_path }}{{ pagename }}{{ suffix }}
-    'github_user': 'apache',
-    'github_repo': 'couchdb-documentation',
-    'github_version': 'master/src/' 
+    "github_user": "apache",
+    "github_repo": "couchdb-documentation",
+    "github_version": "master/src/",
 }
 
-master_doc = 'index'
+master_doc = "index"
 
 text_newlines = "native"
 
-latex_documents = [(
-    "index",
-    "CouchDB.tex",
-    project,
-    "",
-    "manual",
-    True
-)]
+latex_documents = [("index", "CouchDB.tex", project, "", "manual", True)]
 
-latex_elements = {
-    "papersize": "a4paper"
-}
+latex_elements = {"papersize": "a4paper"}
 
-texinfo_documents = [(
-    "contents",
-    "CouchDB",
-    project,
-    "",
-    "CouchDB",
-    "The Apache CouchDB database",
-    "Databases",
-    True
-)]
+texinfo_documents = [
+    (
+        "contents",
+        "CouchDB",
+        project,
+        "",
+        "CouchDB",
+        "The Apache CouchDB database",
+        "Databases",
+        True,
+    )
+]
 
 extlinks = {
-    'issue': ('%s-%%s' % 'https://issues.apache.org/jira/browse/COUCHDB', 'COUCHDB-'),
-    'ghissue': ('https://github.com/apache/couchdb/issues/%s', '#'),
-    'commit': ('https://git-wip-us.apache.org/repos/asf?p=couchdb.git;a=commit;h=%s', '#')
+    "issue": ("%s-%%s" % "https://issues.apache.org/jira/browse/COUCHDB", "COUCHDB-"),
+    "ghissue": ("https://github.com/apache/couchdb/issues/%s", "#"),
+    "commit": (
+        "https://git-wip-us.apache.org/repos/asf?p=couchdb.git;a=commit;h=%s",
+        "#",
+    ),
 }
 
-github_project = 'apache/couchdb-documentation'
+github_project = "apache/couchdb-documentation"
 
-html_context['git_branch'] = github_branch = 'master'
+html_context["git_branch"] = github_branch = "master"
 
-github_docs_path = 'src'
+github_docs_path = "src"
+
 
 def setup(app):
-    app.add_stylesheet('css/rtd_theme.css')
+    app.add_stylesheet("css/rtd_theme.css")
